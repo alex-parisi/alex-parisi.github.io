e7850dfabe5fe9be2053ca36c128f13e135dd6e1
Alex Parisi <alex.parisi@outlook.com>
2022-01-27 16:04:58 -0500

    Initial Commit

---
+++ static/index.js
@@ -0,0 +1,60 @@
+window.onload = () => {
+	$('#sendbutton').click(() => {
+		imagebox = $('#imagebox')
+    imagebox_style = $('#imagebox_style')
+    imagebox_output = $('#imagebox_output')
+		input = $('#input_file')[0]
+    input0 = $('#style_file')[0]
+		if(input.files && input.files[0] && input0.files && input0.files[0])
+		{
+			let formData = new FormData();
+			formData.append('input_filename' , input.files[0]);
+      formData.append('style_filename' , input0.files[0]);
+			$.ajax({
+				url: "http://127.0.0.1:5000/styleImage", // fix this to your liking
+				type:"POST",
+				data: formData,
+				cache: false,
+				processData:false,
+				contentType:false,
+				error: function(data){
+					console.log("upload error" , data);
+					console.log(data.getAllResponseHeaders());
+				},
+				success: function(data){
+					// alert("hello"); // if it's failing on actual server check your server FIREWALL + SET UP CORS
+          console.log("Success");
+					bytestring = data['content']
+					image = bytestring.split('\'')[1]
+					imagebox.attr('src' , 'data:image/jpeg;base64,'+image)
+          bytestring = data['style']
+          image = bytestring.split('\'')[1]
+          imagebox_style.attr('src' , 'data:image/jpeg;base64,'+image)
+          bytestring = data['stylized']
+          image = bytestring.split('\'')[1]
+          imagebox_output.attr('src' , 'data:image/jpeg;base64,'+image)
+				}
+			});
+		}
+	});
+};
+
+
+
+function readUrl(input){
+	imagebox = $('#imagebox')
+	console.log("evoked readUrl")
+	if(input.files && input.files[0]){
+		let reader = new FileReader();
+		reader.onload = function(e){
+			// console.log(e)
+			
+			imagebox.attr('src',e.target.result); 
+			// imagebox.height(300);
+			// imagebox.width(300);
+		}
+		reader.readAsDataURL(input.files[0]);
+	}
+
+	
+}
