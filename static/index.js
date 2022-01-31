a2f74a452c2eca5c35035477248c8b801475de8c
Alex Parisi <alex.parisi@outlook.com>
2022-01-31 00:37:30 -0500

    Finished style transfer web app

--- static/index.js
+++ static/index.js
@@ -1,10 +1,15 @@
 window.onload = () => {
+  $('#loading_icon').hide();
 	$('#sendbutton').click(() => {
 		imagebox = $('#imagebox')
     imagebox_style = $('#imagebox_style')
     imagebox_output = $('#imagebox_output')
 		input = $('#input_file')[0]
     input0 = $('#style_file')[0]
+    content_linker = $('#content_linker')
+    style_linker = $('#style_linker')
+    stylized_linker = $('#stylized_linker')
+    $('#loading_icon').show();
 		if(input.files && input.files[0] && input0.files && input0.files[0])
 		{
 			let formData = new FormData();
@@ -18,21 +23,26 @@ window.onload = () => {
 				processData:false,
 				contentType:false,
 				error: function(data){
+          $('#loading_icon').hide();
 					console.log("upload error" , data);
 					console.log(data.getAllResponseHeaders());
 				},
 				success: function(data){
 					// alert("hello"); // if it's failing on actual server check your server FIREWALL + SET UP CORS
+          $('#loading_icon').hide();
           console.log("Success");
 					bytestring = data['content']
 					image = bytestring.split('\'')[1]
 					imagebox.attr('src' , 'data:image/jpeg;base64,'+image)
+          content_linker.attr('href', 'data:image/jpeg;base64,'+image)
           bytestring = data['style']
           image = bytestring.split('\'')[1]
           imagebox_style.attr('src' , 'data:image/jpeg;base64,'+image)
+          style_linker.attr('href', 'data:image/jpeg;base64,'+image)
           bytestring = data['stylized']
           image = bytestring.split('\'')[1]
           imagebox_output.attr('src' , 'data:image/jpeg;base64,'+image)
+          stylized_linker.attr('href', 'data:image/jpeg;base64,'+image)
 				}
 			});
 		}
